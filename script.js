518e8a712351d118c753a0910edf642ac9ff4da2
Lucas <lucasmires10@gmail.com>
2022-06-20 14:25:14 -0300

    First Release

---
+++ script.js
@@ -0,0 +1,18 @@
+/* function ageTester(a, b){
+    let resultadosubtracao = a - b;
+    return resultadoSoma 
+} */
+
+let ageTester = prompt("Qual será sua idade até o final do ano?")
+let yearAge = 2022
+
+let result = yearAge - ageTester
+
+if (result >= 2000) {
+    alert(`Você nasceu em ${result}.`)
+}else{
+    alert(`Olha não é que o pessoal de ${result} ainda estão na ativa hahaha `)
+}
+
+console.log(result)
+
